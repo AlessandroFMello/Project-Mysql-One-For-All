0a3f4ec62ca363da283761cc8b8784ec6f32252f
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-02-04 05:26:50 -0300

    Requieremt 6: Elaborates min, max, avg and total revenue

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT 
+    ROUND(MIN(planos.valor_plano), 2) AS faturamento_minimo,
+    ROUND(MAX(planos.valor_plano), 2) AS faturamento_maximo,
+    ROUND(AVG(planos.valor_plano), 2) AS faturamento_medio,
+    ROUND(SUM(planos.valor_plano), 2) AS faturamento_total
+FROM
+    SpotifyClone.Planos AS planos
+        INNER JOIN
+    SpotifyClone.Usuarios AS usuarios ON planos.plano_id = usuarios.plano_id;
